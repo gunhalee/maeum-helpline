ceae7a1978730f47431e735103d97c3c92ad18e3
gunhalee <ghlee742@gmail.com>
2026-04-05 08:37:44 +0900

    footer

--- src/components/FooterNote.tsx
+++ src/components/FooterNote.tsx
@@ -11,7 +11,7 @@ export default function FooterNote() {
     <>
       <p>
         {lang === 'en'
-          ? 'This page is to help finding urgent helplines for various crisis situations in Korea.'
+          ? 'This page was created to introduce various urgent helplines in Korea.'
           : '본 페이지는 다양한 긴급상담 기관들을 소개하기 위해 제작되었습니다.'}
       </p>
       <p className="mt-1">
